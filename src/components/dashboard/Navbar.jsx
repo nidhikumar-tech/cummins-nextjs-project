88a9550ce59d7638615a3ac27b0c074d5b6a624d
nidhikumar <nidhi.kumar@onixnet.com>
2025-12-12 16:11:14 +0530

    Disable other navbar buttons while working on use case 1

--- src/components/dashboard/Navbar.jsx
+++ src/components/dashboard/Navbar.jsx
@@ -51,7 +51,10 @@ export default function Navbar() {
           href="/dashboard/heatmap"
           className={styles.navLink}
           onClick={() => setIsMenuOpen(false)}
+          style={{ pointerEvents: "none", opacity: 0.5 }}
+          aria-disabled="true"
         >
+          {/*To enable this button again, remove the style, aria-disabled lines above*/}
           <svg className={styles.navIcon} viewBox="0 0 24 24" fill="currentColor">
             <path d="M3 3h8v8H3V3zm10 0h8v8h-8V3zM3 13h8v8H3v-8zm10 0h8v8h-8v-8z" />
           </svg>
@@ -62,7 +65,10 @@ export default function Navbar() {
           href="/dashboard/chart"
           className={styles.navLink}
           onClick={() => setIsMenuOpen(false)}
+          style={{ pointerEvents: "none", opacity: 0.5 }}
+          aria-disabled="true"
         >
+          {/*To enable this button again, remove the style, aria-disabled lines above*/}
           <svg className={styles.navIcon} viewBox="0 0 24 24" fill="currentColor">
             <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm3.5-9c.83 0 1.5-.67 1.5-1.5S16.33 8 15.5 8 14 8.67 14 9.5s.67 1.5 1.5 1.5z" />
           </svg>
@@ -73,7 +79,10 @@ export default function Navbar() {
           href="/dashboard/graph"
           className={styles.navLink}
           onClick={() => setIsMenuOpen(false)}
+          style={{ pointerEvents: "none", opacity: 0.5 }}
+          aria-disabled="true"
         >
+          {/*To enable this button again, remove the style, aria-disabled lines above*/}
           <svg className={styles.navIcon} viewBox="0 0 24 24" fill="currentColor">
             <path d="M5 9.2h3V19H5zM10.6 5h2.8v14h-2.8zm5.6 8H19v6h-2.8z" />
           </svg>
